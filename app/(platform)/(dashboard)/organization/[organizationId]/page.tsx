052d304da127b71c612fd8be751ae362fc212ddb
SzarneckiP <siarniuk@gmail.com>
2024-04-30 01:40:37 +0200

    db

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -8,11 +8,12 @@ import { checkSubscription } from "@/lib/subscription"
 
 const OrganizationIdPage = async () => {
 
-    const isPro = await checkSubscription()
+    // const isPro = await checkSubscription()
 
     return (
         <div className="w-full mb-20">
-            <Info isPro={isPro} />
+            {/* <Info isPro={isPro} /> */}
+            <Info isPro={true} />
             <Separator className="my-4" />
             <div className="px-2 md:px-4">
                 <Suspense fallback={<BoardList.Skeleton />}>
